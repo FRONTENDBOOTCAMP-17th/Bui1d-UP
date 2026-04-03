781ba3c430ba73e80ea9efdf232772bebd9e44f2
0sliverchoi321z <0sliverchoi321z@gmail.com>
2026-04-03 13:55:06 +0900

    Add: 버튼 컴포넌트 생성 및 테일윈드 설치
    - 테일윈드 설치로 인한 파일 수정
    - 버튼 컴포넌트 생성
    - css 변수를 위한 theme 파일 생성 (현재까지는 기본 색상만 추가)
    - 버튼 컴포넌트를 위한 SVG 파일 추가
    
    Docs: button.html 파일 오타 수정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
