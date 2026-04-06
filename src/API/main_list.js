07da24b2829ea5bcabcb4595178623242a673e3b
bbalabuya <tel030128@naver.com>
2026-04-06 17:31:49 +0900

    홈 리스트 API 반환 수정

--- src/API/main_list.js
+++ src/API/main_list.js
@@ -1,11 +1,23 @@
 export const getMainList = async () => {
   try {
-    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/movies/home`);
+    console.log("Fetching main list from API...");
+    console.log(`accessToken: ${localStorage.getItem("accessToken")}`);
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/movies/home`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      },
+    );
     if (!response.ok) {
       throw new Error("Failed to fetch main list");
     }
-    const data = await response.json();
-    return data;
+    const json = await response.json();
+    console.log("Main list response:", json);
+    return json.data;
   } catch (error) {
     console.error("Error fetching main list:", error);
     return null;
