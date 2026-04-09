05ffbafc9b2524450d76d238944ef42f12515dc5
bbalabuya <tel030128@naver.com>
2026-04-09 11:01:06 +0900

    닉네임 불러오기 API  완성

---
+++ src/API/accountAPI/nickname.js
@@ -0,0 +1,21 @@
+export const getProfileNickname = async () => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/users/me`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      },
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch user profile");
+    }
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+  }
+};
