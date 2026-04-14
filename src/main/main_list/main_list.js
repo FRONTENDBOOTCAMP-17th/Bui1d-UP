8bd1fdf2a6272afe1b4f6ccb66952fd144142bd3
bbalabuya <tel030128@naver.com>
2026-04-14 17:32:41 +0900

    흩어진 키 값 통일

--- src/main/main_list/main_list.js
+++ src/main/main_list/main_list.js
@@ -4,19 +4,7 @@ requireAuth();
 import "./main_list.css";
 import { renderHeader } from "../header/header.js";
 import { getMainList } from "../../API/main_list.js";
-
-const GENRE_MAP = {
-  animation: "애니메이션",
-  comedy: "코미디",
-  romance: "로맨스",
-  action_thriller_crime: "액션 / 스릴러 / 범죄",
-  horror: "호러",
-  sf_fantasy: "SF/판타지",
-  drama: "드라마",
-  documentary: "다큐멘터리",
-  music_musical: "음악/뮤지컬",
-  etc: "기타",
-};
+import { GENRE_MAP } from "@/utils/genres.js";
 
 const GENRE_ORDER = Object.keys(GENRE_MAP);
 
