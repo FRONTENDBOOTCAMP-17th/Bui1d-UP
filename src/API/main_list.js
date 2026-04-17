2c9f2cfafab53e142ae636322e1999050db90a90
영은 <0sliverchoi321z@gmail.com>
2026-04-17 17:21:40 +0900

    Docs: merge conflict 해결 오류 반환

--- src/API/main_list.js
+++ src/API/main_list.js
@@ -2,6 +2,8 @@ import { redirectOnAuthFail } from "@/utils/auth.js";
 
 export const getMainList = async () => {
   try {
+    console.log("Fetching main list from API...");
+    console.log(`accessToken: ${localStorage.getItem("accessToken")}`);
     const response = await fetch(
       `${import.meta.env.VITE_API_BASE_URL}/movies/home`,
       {
@@ -17,7 +19,6 @@ export const getMainList = async () => {
       throw new Error("Failed to fetch main list");
     }
     const json = await response.json();
-    console.log("Main list response:", json);
     return json.data;
   } catch (error) {
     console.error("Error fetching main list:", error);
